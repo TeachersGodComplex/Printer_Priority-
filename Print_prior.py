ece4d35e390bac3a3a8ec1d95b13497155ead12e
Ida <ida@svahndata.se>
2022-05-04 14:13:43 +0200

    g

--- Print_prior.py
+++ Print_prior.py
@@ -12,14 +12,21 @@ EOL = "\n"
 # Class
 
 class Printer:
+    """
+    Class printers innehåller all info om
+    Jag använda denna metod för att returna 
+    namn (str): 
+    tid (int):
+    status 
+    maker 
+    I used this method to return name and printing time. 
+    These values are defined when you choose to use a printer. 
+    """
     def __init__(self, name, time="") -> None:
         self.name = name
         self.time = time
         self.status = False #True if printer, else False
         self.maker = ""
-    
-        def __str__(self) -> str:
-            return f"Det som ska printas när vi printar objektet."
 
     def name(self):
         return self.name
@@ -28,25 +35,45 @@ class Printer:
         return self.time
 
     def check_status(self):
+        """
+        Statusen visas när man öppnar programet och berättar vilken printer som är ledig/upptagen 
+        Om printern används ser man vem som printar och hur lång tid det kommer ta 
+        
+        self.name anges av indexet man väljer i pick_printer
+        self.time/maker anges av användaren i new_print
+        """
         status = ""
         status = "is printing" if self.status else "is free"
-        # Detta visas när man "loggar in" 
-        # Om printern används ser man vem som printar, när den startades samt hur lång tid det kommer ta
         print(f"{self.name} {status}\nPrinting:{self.maker}\nTime:{self.time}")
     
     def new_print(self):
+        """
+        
+        """
         print("")
+        self.status = True
         print(f"Adding new print to {self.name}") # Self.name = namn på vald printer
-        self.maker = input("Who is printing: ") # name == next_in_line???
-        self.start = input("Enter printing time: ") # Kan man skriva in detta i listan 
+        self.maker = input("Who is printing: ") 
+        self.time = input("Enter printing time: ") 
+        next_in_waiting()
         print("Your name is automatically moved from the waitinglist")
-        menu()
-       # next_in_waiting()
+        get_printers()
     
-   # def done_printing(self):
-       # print("")
-       # print(f"{self.maker} are done printing") 
-       # print(f"{self.name} is now free to use") 
+
+    def done_printing(self):
+        """
+        Self.name anges i used_printer, functionen tar bort 
+        maker och time från skrivarn samt ändrar statusen.
+        """
+        print()
+        print(f"{self.maker} is done printing") 
+        print(f"{self.name} is now free to use")
+        # Removes self.maker and self.name from get_printers
+        self.maker = ""
+        self.time = ""
+        self.status = False 
+        print()
+       
 
 # Functions
 
@@ -54,7 +81,8 @@ def options():
     print("(1) See waitinglist")
     print("(2) Add name to waitinglist")
     print("(3) Chose printer to use")
-    # print("(4) Done printing")
+    print("(4) Done printing")
+    print("(#) To exit the program.")
 
 def view(): 
     lines = []
@@ -65,15 +93,20 @@ def view():
     for line in lines:
         count += 1
         print(f'{count}: {line}')
-    menu()
+
 
 def add_to_list():
     with open("waitinglist.txt", "a", encoding="utf-8") as waiting_list:
         name = input("Enter name:") # Skriv in ditt namn i listan 
         waiting_list.write(name+"\n")
-    menu()
+    get_printers()
+    
 
 def pick_printer():
+    """
+    Ger printrarna ett index.
+    Valet beslutar vilken printer man vill använda.
+    """
     print()
     print("Choose a printer:")
     print("Pick the index you want to use.")
@@ -83,36 +116,68 @@ def pick_printer():
     pick = input(">>> ")
     return printers[int(pick)]
 
-# def picked_printer():
-   # print("Choose used printer:")
-   # print("Pick the index you have used.")
-   # for i, printed in enumerate(printers):
-    #    print(f"{i}, {printed.name}")
-   # pick = input(">>> ")
-   # return printers[int(pick)]
-    
+
+def used_printer():
+    """
+    Samma function som def pick_printer utom texten som printas
+    Detta index används dock för att identifiera vilken printer man ska 
+    ta bort en print ifrån.
+    """
+    print()
+    print("Choose used printer:")
+    print("Pick the index you have used.")
+    for i, printer in enumerate(printers):
+        print(f"{i}, {printer.name}")
+    pick = input(">>> ")
+    return printers[int(pick)]
+
+
 def menu():
-    print("""----Options----""")
-    options()
-    choice = input(">>> ")
-    if choice == "1":
-        view()
+    """
+    En def kallas på genom if-satsen av ett input som är lika med ett choice.
+    Choice definerars av inputet 1-4 eller #
+    """
+    choice = ""
+    while choice != "#":
+    # När choice är "#" avsluta programet annars printa menu
+    # Genom att sätta menyn i en loop slipper jag kalla på den efter varje def
+        print("""----Options----""")
+        options()
+        choice = input(">>> ")
+        if choice == "1":
+            view()
 
-    elif choice == "2":
-        add_to_list()
+        elif choice == "2":
+            add_to_list()
 
-    elif choice == "3":
-        printer = pick_printer()
-        printer.new_print()
+        elif choice == "3":
+            # Tar vald printer och sätter den som self.name
+            # i efterföljande def vet programet under 
+            # vilken printer den ska ändra infon och statusen
+            printer = pick_printer()
+            printer.new_print()
 
-    # elif choice == "4":
-       # printed = picked_printer()
-       # printed.done_printing()
-    
-    else:
-        print("Incorrect input")
+        elif choice == "4":
+            printer = used_printer()
+            printer.done_printing()
+
+        elif choice == "#":
+            # Avslutar loppen och programet
+            print("Goodbye!")
+
+        else:
+            # Felaktigt input gör att menyn loppas  
+            print("Incorrect input")
+
 
 def next_in_waiting():
+    """
+    I väntelistan numreras alla namn, functionen plockar bort indexet nr1 
+    varje gång någon skriver in ett namn på en av printrarna.
+    Nr 1 i väntelistan skriver in sitt namn på en printer 
+    och tas samtidigt bort från väntelistan. För att det ska fungera 
+    måste folk respektera ordningen på väntelistan.
+    """
     waiters = []
     with open("waitinglist.txt", "r", encoding="utf-8") as f:
         waiters = f.readlines()
@@ -121,12 +186,14 @@ def next_in_waiting():
         for line in waiters:
             f.write(line)
 
+
 def get_printers():
     print("----Printers----")
     # Visar vilka printers som är lediga Free/Printing
     for printer in printers:
         printer.check_status()
         print()
+
 
 def main():
     #global printers
@@ -138,12 +205,6 @@ def main():
     print("Following printers are online:")
     for printer in printers:
         print(printer.name)
-
-    print("""
-If your turn, choose printer 
-and fill in required info
-(Input ALWAYS 1-3)
-""")
 
     get_printers()
     menu()
--- README.md
+++ README.md
@@ -1,39 +1,37 @@
 # Printer_Priority
-A log program for the printers. A person will be able to see if any printers are free or add themself on a waitinglist. 
+To effectively use the printors
+With all the information at hand more people will find time to use the printers. It also makes it easy to trace back errors or broken parts to one person.
 
+## Description
 
-## Why should I use this project 
-In order to effectively use the printors I'm creating a program to log everything. With a waitinglist and all the information at hand more people will find time to use the printers. It also makes it easy to trace back errors or broken parts to one person.
+## Språk
+Phyton
 
+Phyton 3.7+
 
-### Setup
-As of now everything takes place in the terminal
+## Installation
+Github repository:
 
+PEP8
+EOL = 
 
-## 30/3 
-### Startup
-Planing out what functions I wanted and how things are gonna be displayed 
+## Usage
 
+## Roadmap
+-[x]
+-[x]
+-[x]
+-[]
 
-## 6/4 
-### Class Printer:
-I made a class for the printers. Here all the info about them and the person printing can be found.
+## Contribution
+This task have jet to be graded and there for pull request will not be aproved. As soon it's been graded this will be alowed. 
 
-### check_status 
-When logging in following printers will get a status check to see wich ones are being used. 
+[MIT] (https://...)
 
-### def__init__(self)
-I used this method to return name and printing time. These values are defined when you choose to use a printer. 
+## Contact
+Discord: 
+Mail: Ida@svahndata.se
 
+## Acknowledgements
+- Niclas Lund
 
-## 20/4
-I changed the menu and now there are 3 options and three def functions. A function is called on through the if-statement by an input. The input is defined as a choice.
-
-### Displayed in cod:  
-choice = input(">>> ") 
-
-
-def pick_printer hur den fungerar mm.
-
-## 27/4 
-def next_in_waiting takes away the 0 index value from the waitinglist (Index 0 = First name on list) everytime someone adds there name to a printer. This function makes the program a lot more automatic and calls for less steps when using the printers.  
